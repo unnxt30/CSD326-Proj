c3ffd1743e786af68946b89b8a203c4aeb7a7997
Unnat Sharma <officialunnat30@gmail.com>
2025-04-24 22:54:40 +0530

    chore: shift codebase

--- codebase/src/pages/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInForm } from '../components/SignInForm';
-
-export default function SignIn() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
-      <SignInForm />
-    </div>
-  );
-} 
